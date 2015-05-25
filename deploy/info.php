cc00beed8646ba9541d8cf823acf8ba6859e640f
Benjamin Chambers <bchambers@clearfoundation.com>
2015-05-25 16:22:48 -0400

    Bumped version for packaging

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'mail_routing';
-$app['version'] = '2.0.24';
+$app['version'] = '2.0.25';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
