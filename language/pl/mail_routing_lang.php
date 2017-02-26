5b7f44d85cc551302aca96c10403a4138d9e6fd0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:49:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Poczta Routing';
+$lang['mail_routing_app_description'] = 'Mail Routing zapewnia sprawie rozpatrywania różnych aplikacjach pocztowych trasie plików.';
