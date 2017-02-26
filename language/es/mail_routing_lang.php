cc40f62926171abb7ab72b81a05ffeb500f19032
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:50:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'el enrutamiento de correo';
+$lang['mail_routing_app_description'] = 'El enrutamiento de correo ofrece en el manejo en diversas aplicaciones de correo ruta del archivo.';
