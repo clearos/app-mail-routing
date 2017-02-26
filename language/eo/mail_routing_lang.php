86f31a8a7e2805066d17c3e0bb47206c9d7f7d4c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:43:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'poŝto Routing';
+$lang['mail_routing_app_description'] = 'La Poŝto Routing provizas sur la dosiero itinero uzado por diversaj poŝto apps.';
