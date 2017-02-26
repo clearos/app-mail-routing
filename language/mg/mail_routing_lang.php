5ef3a8e32ede16dba30ff1f3f99ae72fa77acb64
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:47:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Mail tambazotra';
+$lang['mail_routing_app_description'] = 'Ny Mail tambazotra dia manome ny rakitra lalana manazava ny fampiharana namany Sary isan-karazany.';
