706af1544eb82c89d72d674541e7b17d11f91b00
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:47:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Ararere mēra';
+$lang['mail_routing_app_description'] = 'whakarato te Mail Ararere i runga i te huarahi kōnae te whāwhā hoki ngā taupānga mēra.';
