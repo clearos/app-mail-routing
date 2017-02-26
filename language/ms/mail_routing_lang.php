f5dea9939e3c88fbc173d8069d1074d41650e340
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:48:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Routing mail';
+$lang['mail_routing_app_description'] = 'The Mail Routing menyediakan laluan fail ini pengendalian pelbagai aplikasi mel.';
