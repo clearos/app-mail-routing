43a92e2a47ccd7eed3a6c0de3b89be11677db6f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:47:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'pasts maršrutēšana';
+$lang['mail_routing_app_description'] = 'Pasta maršrutēšanas sniedz uz faila maršrutā apstrādes dažādu pasta progr.';
