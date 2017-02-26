885067e1522f2182b53d3351217bdf137d39e284
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:47:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'paštas Maršrutai';
+$lang['mail_routing_app_description'] = 'Paštas Maršrutai suteikia ant failo maršrutas ruošti įvairių pašto programų.';
