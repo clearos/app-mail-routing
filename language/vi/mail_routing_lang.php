7ccb45d0747d198f76c38d57cf8130547fcb847d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:52:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Routing thư';
+$lang['mail_routing_app_description'] = 'The Mail Routing cung cấp trên các tuyến đường xử lý tập tin cho các ứng dụng email khác nhau.';
