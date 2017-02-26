62954da97a00e3c9be812a0724a6399dd0f58087
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:50:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Mail Routing';
+$lang['mail_routing_app_description'] = 'The Mail Routing fana ka tsamaisetsa faele ea tšoarang bakeng ditiriso tse fapa-fapaneng mail.';
