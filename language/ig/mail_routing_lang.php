bbec1e998114ddad560a4a3694e0ba7710aea1c9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:45:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'mail nsochi usoro';
+$lang['mail_routing_app_description'] = 'The Mail nsochi usoro na-enye on the file ụzọ na-ejizi ihe dị iche iche mail ngwa.';
