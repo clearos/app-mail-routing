9dddf6b5a4261fa4bc564b481f27697969ba748c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:43:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Mail směrování';
+$lang['mail_routing_app_description'] = 'Mail směrování poskytuje informací o vyřizování různých mailových aplikací trase souboru.';
