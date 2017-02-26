b657baa85d8c042358192a6163b643b91515d1fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:52:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'פּאָסט רוטינג';
+$lang['mail_routing_app_description'] = 'די פּאָסט רוטינג גיט אויף די טעקע מאַרשרוט האַנדלינג פֿאַר פאַרשידן פּאָסט אַפּס.';
