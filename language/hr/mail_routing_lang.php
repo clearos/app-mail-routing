53a030696a5045b0af17312e353f71eba2ce27df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:43:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'mail Routing';
+$lang['mail_routing_app_description'] = 'The Mail Routing daje na datotečnom relaciji rukovanje za različite aplikacije e-pošte.';
