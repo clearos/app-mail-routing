c824d57a304aedcf114730566404a3634e2c9464
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:45:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Mail Routing';
+$lang['mail_routing_app_description'] = 'Soláthraíonn an Routing Mail ar an mbealach comhad láimhseáil do apps ríomhphoist éagsúla.';
