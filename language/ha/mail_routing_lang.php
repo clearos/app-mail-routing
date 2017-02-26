f1f2810b77ae62db390437b9350f74a57040241d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:44:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Mail bayar da kwatance';
+$lang['mail_routing_app_description'] = 'The Mail bayar da kwatance bayar a cikin fayil hanya tanadin domin daban-daban mail apps.';
