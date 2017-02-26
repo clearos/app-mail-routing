cd619f89ad84fb18bca77129d550c33a9b3793c0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:43:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Mail yolozera';
+$lang['mail_routing_app_description'] = 'The Mail yolozera limapereka njira file wolunjika kwa mapulogalamu osiyanasiyana makalata.';
