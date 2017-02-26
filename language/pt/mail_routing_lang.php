0ef7ed3287c876f901ef7cb2094fbfae22a3ec66
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:49:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'roteamento de correio';
+$lang['mail_routing_app_description'] = 'O roteamento de emails fornece na rota manipulação de arquivos para vários aplicativos de e-mail.';
