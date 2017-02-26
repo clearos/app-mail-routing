2d83baeb7369c9225b425d359aaa45ab77e0df3f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:42:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'မေးလ် Routing';
+$lang['mail_routing_app_description'] = 'အဆိုပါမေးလ် Routing အမျိုးမျိုးသောမေးလ် apps များအဘို့ကိုင်တွယ်ဖိုင်ကိုလမ်းကြောင်းအပေါ်ပေးစွမ်းသည်။';
