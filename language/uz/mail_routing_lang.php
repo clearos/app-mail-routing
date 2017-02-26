afe288605ddd82281ee0ad5fade6c7af8fc81bd6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:52:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'mail Routing';
+$lang['mail_routing_app_description'] = 'Mail Marshrutlashni turli mail ilovalar uchun ishlash fayl yo&#39;nalishi bo&#39;yicha beradi.';
