e86e2b54d5993cc026b89220e78f61fc45dd87de
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:52:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Llwybro Mail';
+$lang['mail_routing_app_description'] = 'Mae&#39;r Llwybro Mail yn darparu ar y trafod ar gyfer apps post amrywiol llwybr ffeil.';
