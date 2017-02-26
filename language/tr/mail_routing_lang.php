2dc701e4c0226489af87d80b5f579900a8424067
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:52:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'posta Yönlendirme';
+$lang['mail_routing_app_description'] = 'Posta Yönlendirme çeşitli posta uygulamaları için işleme dosya yolu üzerinde sağlar.';
