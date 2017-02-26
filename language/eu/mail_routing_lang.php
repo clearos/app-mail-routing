3525ce88ba13f42485e800e45acdf31737086db3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:42:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Mail Bideratze';
+$lang['mail_routing_app_description'] = 'Mail Bideratze dute fitxategi ibilbidea hainbat mail apps for manipulazioa eskaintzen.';
