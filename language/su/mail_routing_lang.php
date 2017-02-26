90b8e421bb8569ce22a85c57463cd190d0fd46b4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:50:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'surat Routing';
+$lang['mail_routing_app_description'] = 'Cijeungjing Routing nyadiakeun dina rute file nanganan pikeun sagala rupa aktip mail.';
