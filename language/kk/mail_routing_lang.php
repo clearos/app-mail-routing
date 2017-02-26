d1d784e9945f63392b7df069f9fa532364b52c20
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:46:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/mail_routing_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['mail_routing_app_name'] = 'Mail маршруттау';
+$lang['mail_routing_app_description'] = 'Пошта Маршруттау түрлі пошта қолданбалар үшін өңдеу файл маршрутта қамтамасыз етеді.';
